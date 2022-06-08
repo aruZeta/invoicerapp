37d18ea34f18e5895f2b81391e67bf9236ef3e3a
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-06-08 19:14:10 +0200

    [add] Sample index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import './styles/main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+);
+
+const element = <h1>Hello, world</h1>;
+
+root.render(element);

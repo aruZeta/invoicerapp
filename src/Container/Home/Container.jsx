7c5372c2a58e44456841b7a23875d0e1a31a6c13
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-06-09 15:00:03 +0200

    [refactor] Surroinding links with a LinkContainer

--- src/Container/Home/Container.jsx
+++ src/Container/Home/Container.jsx
@@ -1,11 +1,12 @@
 import { Button, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const MyHomeContainer = () => {
     return (
         <Container fluid="lg" className="flex-fill d-flex">
-            <a href="invoices/new" className="m-auto">
+            <LinkContainer to="/invoices/new" className="m-auto">
                 <Button variant="success">New Invoice</Button>
-            </a>
+            </LinkContainer>
         </Container>
     );
 };

811afa5a27272c82fc38ba9406944a625defacf7
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-06-10 14:57:25 +0200

    [refactor] Check if extra only once and better gap

--- src/Routes/Invoices/New/Container/InvoicePage.jsx
+++ src/Routes/Invoices/New/Container/InvoicePage.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { Col, Form, Row } from "react-bootstrap";
 
 import InvoicePageHeader from "./InvoicePageHeader";
@@ -7,14 +8,19 @@ const border = "border border-4 border-dark";
 const rounded = "rounded rounded-4";
 
 const InvoicePage = ({extra}) =>
-<Form className={`invoice mx-auto ${border} ${rounded}`}>
-    <Row xs={1} className="mx-0">
-        <Col className="my-3">
-            {extra ? null : <InvoicePageHeader />}
-        </Col>
-        <Col>
-            {extra ? null : <InvoicePageSubHeader />}
-        </Col>
+<Form className={`invoice py-3 mx-auto ${border} ${rounded}`}>
+    <Row xs={1} className="mx-0 gap-3">
+        { extra
+          ? null
+          : <Fragment>
+                <Col>
+                    <InvoicePageHeader />
+                </Col>
+                <Col>
+                    <InvoicePageSubHeader />
+                </Col>
+            </Fragment>
+        }
     </Row>
 </Form>;
 

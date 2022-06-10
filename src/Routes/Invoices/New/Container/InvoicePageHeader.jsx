a79a99e6a2ce9a97cef1559131afd79e71805989
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-06-10 13:33:54 +0200

    [refactor] Removing some nesting

--- src/Routes/Invoices/New/Container/InvoicePageHeader.jsx
+++ src/Routes/Invoices/New/Container/InvoicePageHeader.jsx
@@ -10,6 +10,40 @@ const MyInput = ({type, placeholder, className, required}) =>
     required={required ? true : false}
 />;
 
+const HeaderDetails = () =>
+<Card.Body className="p-2 d-flex flex-column gap-2">
+    <Form.Group>
+        <MyInput type="text" placeholder="Address" />
+    </Form.Group>
+    <div className="d-flex gap-2">
+        <Form.Group style={{flex: "1 1 100%"}}>
+            <MyInput type="text" placeholder="Town/City" />
+        </Form.Group>
+        <Form.Group>
+            <MyInput className="text-end" type="text" placeholder="CP" />
+        </Form.Group>
+        <Form.Group>
+            <MyInput className="text-end" type="text" placeholder="Fecha" />
+        </Form.Group>
+    </div>
+    <div className="d-flex gap-2">
+        <Form.Group style={{flex: "1 1 200%"}}>
+            <MyInput type="text" placeholder="Small Description" />
+        </Form.Group>
+        <Form.Group>
+            <MyInput className="text-end" type="text" placeholder="N°" />
+        </Form.Group>
+    </div>
+    <div className="d-flex gap-2">
+        <Form.Group style={{flex: "0 1 35%"}}>
+            <MyInput className="" type="tel" placeholder="Telephone" />
+        </Form.Group>
+        <Form.Group className="flex-fill">
+            <MyInput className="text-end" type="email" placeholder="Email" />
+        </Form.Group>
+    </div>
+</Card.Body>;
+
 const InvoicePageHeader = () =>
 <Row>
     <Col xs={5}>
@@ -17,38 +51,7 @@ const InvoicePageHeader = () =>
     </Col>
     <Col xs={7}>
         <Card bg="light" className="w-100 h-100">
-            <Card.Body className="p-2 d-flex flex-column gap-2">
-                <Form.Group>
-                    <MyInput type="text" placeholder="Address" />
-                </Form.Group>
-                <div className="d-flex gap-2">
-                    <Form.Group style={{flex: "1 1 100%"}}>
-                        <MyInput type="text" placeholder="Town/City" />
-                    </Form.Group>
-                    <Form.Group>
-                        <MyInput className="text-end" type="text" placeholder="CP" />
-                    </Form.Group>
-                    <Form.Group>
-                        <MyInput className="text-end" type="text" placeholder="Fecha" />
-                    </Form.Group>
-                </div>
-                <div className="d-flex gap-2">
-                    <Form.Group style={{flex: "1 1 200%"}}>
-                        <MyInput type="text" placeholder="Small Description" />
-                    </Form.Group>
-                    <Form.Group>
-                        <MyInput className="text-end" type="text" placeholder="N°" />
-                    </Form.Group>
-                </div>
-                <div className="d-flex gap-2">
-                    <Form.Group style={{flex: "0 1 35%"}}>
-                        <MyInput className="" type="tel" placeholder="Telephone" />
-                    </Form.Group>
-                    <Form.Group className="flex-fill">
-                        <MyInput className="text-end" type="email" placeholder="Email" />
-                    </Form.Group>
-                </div>
-            </Card.Body>
+            <HeaderDetails />
         </Card>
     </Col>
 </Row>;

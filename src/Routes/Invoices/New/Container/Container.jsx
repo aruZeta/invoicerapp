91a98db77a952438b619f3e3ef04aa2eba937b64
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-06-10 13:30:00 +0200

    [refactor, add] Button to add more pages to the invoice

--- src/Routes/Invoices/New/Container/Container.jsx
+++ src/Routes/Invoices/New/Container/Container.jsx
@@ -1,15 +1,48 @@
-import { Col, Container, Row } from 'react-bootstrap';
+import { Fragment, useState } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
-import Invoice from './Invoice';
+import InvoicePage from './InvoicePage';
+import InvoicePageExtra from './InvoicePageExtra';
 
-const NewInvoice = () =>
-<Col className="mb-4"><Invoice /></Col>;
+const MyContainer = () => {
+    const [invoicePagesCount, setInvoicePagesCount] = useState(() => {
+        return 1;
+    });
 
-const MyContainer = () =>
-<Container fluid className="flex-fill">
-    <Row xs={1} className="mt-4">
-        <NewInvoice />
-    </Row>
-</Container>;
+    const addInvoicePage = () => {
+        setInvoicePagesCount(invoicePagesCount + 1);
+    };
+
+    const handleClick = () => {
+        return () => addInvoicePage();
+    }
+
+    const invoicePagesExtra = () => {
+        const invoicePages = [];
+        for (let i = 1; i < invoicePagesCount; i++) {
+            invoicePages.push(
+                <Col key={i} className="mb-4">
+                    <InvoicePageExtra />
+                </Col>
+            )
+        }
+
+        return invoicePages;
+    }
+
+    return (
+        <Container fluid className="flex-fill">
+            <Row xs={1} className="mt-4 mb-4">
+                <Col className="mb-4"><InvoicePage /></Col>
+                {invoicePagesExtra()}
+                <Col>
+                    <Button onClick={handleClick()} variant="dark" className="new-invoice-btn mx-auto d-block">
+                        New Invoice
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
 export default MyContainer;
